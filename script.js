9a354b9f362056286c9a38ef935635a94f4f3407
Kunal Tankra <kunaltankra2002@gmail.com>
2023-03-29 10:13:38 +0530

    finished

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const breakDeductions = document.getElementsByClassName("breakDeduction")
 // caculate function event.......
 const clickCalculate=(e)=>{
     e.preventDefault()
+    let totalHour = 0,  totalMin = 0;
 
     for (let ind = 0; ind<startTimes.length; ind++) {
         const startTime = startTimes[ind].children
@@ -32,42 +33,60 @@ const clickCalculate=(e)=>{
         let breakHour = parseInt(breakDeduction[0].value);
         let breakMin = parseInt(breakDeduction[1].value);
 
-        if(start_AM_PM == "PM"){
-            startHour += 12;
-        }
-
-        if(end_AM_PM == "PM"){
-            endHour += 12;
-        }
-
+        
 
         let resHour;
         let resMin;
         let calC
-        if(start_AM_PM == "PM" && end_AM_PM == "AM"){
-            calC = calCTime(endHour, endMin,startHour, startMin);
-            resHour= calC[0];
-            resMin = calC[1];
-
-            calC = calCTime(resHour,resMin,24,0);
-            resHour = calC[0];
-            resMin = calC[1];
-            
+        
+        if(startHour == 0 || endHour == 0){
+            resHour = "0"
+            resMin = "00"
         }
         else{
-            calC = calCTime(startHour, startMin, endHour, endMin);
+            if(start_AM_PM == "PM"){
+                startHour += 12;
+            }
+    
+            if(end_AM_PM == "PM"){
+                endHour += 12;
+            }
+
+            if(start_AM_PM == "PM" && end_AM_PM == "AM"){
+                calC = calCTime(endHour, endMin,startHour, startMin);
+                resHour= calC[0];
+                resMin = calC[1];
+                
+                calC = calCTime(resHour,resMin,24,0);
+                resHour = calC[0];
+                resMin = calC[1];
+                
+            }
+            else{
+                calC = calCTime(startHour, startMin, endHour, endMin);
+                resHour = calC[0];
+                resMin = calC[1];
+                
+            }
+            
+            calC = calCTime(breakHour, breakMin, resHour, resMin);
             resHour = calC[0];
             resMin = calC[1];
-            
         }
-        
-        calC = calCTime(breakHour, breakMin, resHour, resMin);
-        resHour = calC[0];
-        resMin = calC[1];
 
-        console.log(resHour, resMin)
+      
+        totalOfRow.innerHTML = `${resHour}.${resMin == 0? "00": resMin}`
+
+        totalHour += parseInt(resHour);
+        totalMin += parseInt(resMin);
     }
+
+    totalHour += Math.floor(totalMin/60);
+    totalMin = totalMin%60;
+
+    totalHours.innerHTML = `${totalHour}.${totalMin == 0? "00": totalMin}`
 }
+
 
 // calculate the time
 const calCTime = (stHour,stMin,endHour,endMin)=>{  
@@ -91,13 +110,27 @@ const clickClearAll=(e)=>{
 
     // set 0
     for (const idx in setHour) {
-        setHour[idx].value = "";
-        setMin[idx].value = "";
+        setHour[idx].value = "0";
+        setMin[idx].value = "00";
     }
 
     for (const elem of totalOfRows) {
         elem.innerHTML = "- -"
     }
+
+    // set am in pm
+    for (let ind = 0; ind<startTimes.length; ind++) {
+        const startTime = startTimes[ind].children
+        const endTime = endTimes[ind].children
+        
+        // start time
+        startTime[2].value = "AM";
+        
+        // end time
+        endTime[2].value = "PM";
+        
+    }
+
 
     totalHours.innerText = "- -"
 }
